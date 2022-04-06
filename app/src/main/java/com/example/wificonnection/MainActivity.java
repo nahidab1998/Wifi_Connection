ddddc46c19291f4395bc0f8dc97c892611c32183
nahid.ab <n.alibagheri1998@gmail.com>
2022-04-06 13:23:20 +0430

    fix design

--- app/src/main/java/com/example/wificonnection/MainActivity.java
+++ app/src/main/java/com/example/wificonnection/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.wificonnection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView send , connecte , list_device ;
+    private TextView massage ;
+    private EditText editText ;
+    private ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initId();
+        
+
+    }
+
+    private void initId() {
+        send = findViewById(R.id.bt_send);
+        connecte = findViewById(R.id.bt_listen);
+        list_device = findViewById(R.id.bt_listdevice);
+        editText = findViewById(R.id.editxt_message);
+        massage = findViewById(R.id.txt_message);
+        listView = findViewById(R.id.listview_device);
     }
 }
